736edc95af2e09ac134b4a3838f1a8588bd54a86
ibayraktar <111bayraktar@gmail.com>
2026-02-19 03:01:04 +0000

    new lesson2

---
+++ ibayraktar02.py
@@ -0,0 +1,44 @@
+#ibayraktar02
+#
+
+bit1 =int(input("Enter the first bit (0 or 1)!"))
+bit2=int(input("Enter the second bit (0 or 1)!"))
+bit3=int(input("Enter the third bit (0 or 1)!"))
+bit4=int(input("Enter the fourth bit (0 or 1)!"))
+bit5=int(input("Enter the fith bit (0 or 1)!"))
+bit6=int(input("Enter the sixth bit (0 or 1)!"))
+
+if (bit1==0):
+    b1=0
+if (bit1==1):
+    b1=1
+if (bit2==0):
+    b2=0
+if (bit2==1):
+    b2=2
+if (bit3==0):
+    b3=0
+if (bit3==1):
+    b3=4
+if (bit4==0):
+    b4=0
+if (bit4==1):
+    b4=8
+if (bit5==0):
+    b5=0
+if (bit5==1):
+    b5=16
+if (bit6==0):
+    b6=0
+if (bit6==1):
+    b6=32
+
+print ("Your decimal number is ", (b1+b2+b3+b4+b5+b6))
+    
+ 
+
+ 
+ 
+
+
+
